fcc8566814cbc848d337b2d8e26da8ac4c99d080
eunchaejulia <eunchaejulia1029@naver.com>
2025-06-01 14:57:09 +0900

    Add files via upload

--- api/chat.js
+++ api/chat.js
@@ -14,7 +14,7 @@ module.exports = async (req, res) => {
 AI:
   `;
 
-  const response = await fetch("https://api-inference.huggingface.co/models/tiiuae/falcon-rw-1b", {
+  const response = await fetch("https://api-inference.huggingface.co/models/bigscience/bloomz-560m", {
     method: "POST",
     headers: {
       Authorization: `Bearer ${process.env.HF_API_KEY}`,
